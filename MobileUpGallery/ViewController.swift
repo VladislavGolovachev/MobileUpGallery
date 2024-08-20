ce39bfbb856ce742f2505adbab1c71cb905e1fff
VladislavGolovachev <serpentess14@gmail.com>
2024-08-20 15:04:48 +0500

    Исходный коммит. Добавлен файл .gitignore.

---
+++ MobileUpGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileUpGallery
+//
+//  Created by Владислав Головачев on 20.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

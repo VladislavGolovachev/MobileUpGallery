7df443b0ac6281827c6367dcd8f85f03ee76a258
VladislavGolovachev <serpentess14@gmail.com>
2024-08-21 22:02:41 +0500

    Добавлена навигация посредством Router

--- MobileUpGallery/Router.swift
+++ MobileUpGallery/Router.swift
@@ -8,9 +8,50 @@
 import UIKit
 
 protocol RouterProtocol {
-    
+    init(navigationController: UINavigationController, moduleBuilder: ModuleBuilderProtocol)
+    func initiateRootViewController()
+    func showWebViewController()
+    func goToMainViewController()
+    func goToPhotoViewController()
+    func goToVideoViewController()
+    func popToRootViewController()
 }
 
 final class Router: RouterProtocol {
+    var navigationController: UINavigationController
+    var moduleBuilder: ModuleBuilderProtocol
     
+    init(navigationController: UINavigationController, moduleBuilder: ModuleBuilderProtocol) {
+        self.navigationController = navigationController
+        self.moduleBuilder = moduleBuilder
+    }
+    
+    func initiateRootViewController() {
+        let authVC = moduleBuilder.createAuthModule(router: self)
+        navigationController.viewControllers = [authVC]
+    }
+    
+    func showWebViewController() {
+        let webVC = WebViewController()
+        navigationController.viewControllers.first?.present(webVC, animated: true)
+    }
+    
+    func goToMainViewController() {
+        let mainVC = moduleBuilder.createMainModule(router: self)
+        navigationController.pushViewController(mainVC, animated: true)
+    }
+    
+    func goToPhotoViewController() {
+        let photoVC = moduleBuilder.createPhotoModule(router: self)
+        navigationController.pushViewController(photoVC, animated: true)
+    }
+    
+    func goToVideoViewController() {
+        let videoVC = moduleBuilder.createVideoModule(router: self)
+        navigationController.pushViewController(videoVC, animated: true)
+    }
+    
+    func popToRootViewController() {
+        navigationController.popToRootViewController(animated: true)
+    }
 }
